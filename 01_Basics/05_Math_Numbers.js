de502bdd27211332e8e9aa40715ad4d49f1429d2
Priyanshu Nautiyal <137296498+Priyanshu02333@users.noreply.github.com>
2024-04-08 13:31:49 +0000

    Math and Numbers

---
+++ 01_Basics/05_Math_Numbers.js
@@ -0,0 +1,9 @@
+//+++++++++++++++ MATH ++++++++++++++
+console.log(Math); // it is an object withdifferent inbuilt properrties in it
+console.log(Math.random());
+console.log(Math.floor((Math.random() *10) + 1) );
+// if you want random number between a range do follow bellow steps
+const min = 10;
+const max = 20;
+console.log(Math.floor(Math.random() * (max - min +1) + min ));
+
