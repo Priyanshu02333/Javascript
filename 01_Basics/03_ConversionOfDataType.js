8584599f2ce0c92d0f550852af0db14f43668280
Priyanshu Nautiyal <137296498+Priyanshu02333@users.noreply.github.com>
2024-04-08 11:58:12 +0000

    String commit

--- 01_Basics/03_ConversionOfDataType.js
+++ 01_Basics/03_ConversionOfDataType.js
@@ -1,23 +1,38 @@
-//Conversion in Number
-let score = "3abc" //string 
-let numberscore = Number(score); // number
-console.table([ typeof score , typeof numberscore , numberscore]);
-/*
-"33" => 33 number
-"33abc" => Nan (Not a number);
-true => 1; false => 0
-*/ 
-//Boolean Convergion 
-let isLogged = 1;
-let Inboolean = Boolean(isLogged);
-console.table([ typeof isLogged , typeof Inboolean , Inboolean]);
-/*
-1 => true
-0 => false
-"" => false
-"Priyanshu" => true
-*/
-//String Conversion
-let a = 33; // number
-let aInString = String(a); // number
-console.table([ typeof a , typeof aInString , aInString]);
+// //Conversion in Number
+// let score = "3abc" //string 
+// let numberscore = Number(score); // number
+// console.table([ typeof score , typeof numberscore , numberscore]);
+// /*
+// "33" => 33 number
+// "33abc" => Nan (Not a number);
+// true => 1; false => 0
+// */ 
+// //Boolean Convergion 
+// let isLogged = 1;
+// let Inboolean = Boolean(isLogged);
+// console.table([ typeof isLogged , typeof Inboolean , Inboolean]);
+// /*
+// 1 => true
+// 0 => false
+// "" => false
+// "Priyanshu" => true
+// */
+// //String Conversion
+// let a = 33; // number
+// let aInString = String(a); // number
+// console.table([ typeof a , typeof aInString , aInString]);
+
+// Operation //
+// complex situation
+// console.log("1" + 2 + 2); // 122
+// console.log( 1 + 2 +"3" ) // 33
+// //prefix and postfix 
+// let g = 3;
+// let h = g++;
+// console.log(g);
+// console.log(h);
+// postfix 
+let g = 3;
+ let h = ++g;
+ console.log(g);
+console.log(h);
---
+++ 01_Basics/04_String.js
@@ -0,0 +1,22 @@
+// Documentation on String 
+// const name1 = "Priyanshu"
+// const repCount = 5 
+// // best way to write code 
+// console.log(`My Name is ${name1} and my RepoCount till now is ${repCount}`);
+
+// Declaration of string 
+// const name1 = new String('     Priyanshu');
+// // different types of Methods we can perform on my string
+// console.log(name1.length); // 9
+// console.log(name1.toLowerCase()); // priyanshu
+// console.log(name1.charAt((2))); //i
+// console.log(name1.indexOf('i')); // 2
+// console.log(name1.trim());
+
+const url = "https//priyanshu.com//nauty??";
+console.log(url);
+console.log(url.replace('priyanshu' , 'PrinautsIndustry'));
+console.log(url.includes('.com'));
+
+// For resorce or method on string
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String
