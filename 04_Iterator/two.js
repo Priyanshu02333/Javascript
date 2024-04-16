831913b9754018a5715128793d7965c757dd3aa0
Priyanshu Nautiyal <137296498+Priyanshu02333@users.noreply.github.com>
2024-04-16 11:55:01 +0000

    While and Do While loop

---
+++ 04_Iterator/two.js
@@ -0,0 +1,14 @@
+//************* While & do while looo************************/
+// let score = 0
+
+// while (score <= 10) {
+//     //console.log(`Score is : ${score}`);
+//     score = score+2
+// }
+
+// Do While loop
+let score = 12;
+do {
+    console.log(` Your SCore is : ${score}`);
+    score =score+1;
+} while (score <= 10);
